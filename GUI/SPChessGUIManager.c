1c80c012e75a024b6c8e0303dc5193ad1ed06c4f
omerk1 <omerk1@mail.tau.ac.il>
2017-09-26 13:54:11 +0300

    Update SPChessGUIManager.c

--- GUI/SPChessGUIManager.c
+++ GUI/SPChessGUIManager.c
@@ -1,71 +1,116 @@
-//#include "SPChessGUIManager.h"
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//SPGuiManager* spManagerCreate() {
-//	SPGuiManager* res = (SPGuiManager*) malloc(sizeof(SPGuiManager));
-//	if (res == NULL ) {
-//		return NULL ;
-//	}
-//	res->mainWin = spMainWindowCreate();
-//	if (res->mainWin == NULL ) {
-//		free(res);
-//		return NULL ;
-//	}
-//	res->gameWin = NULL;
-//	res->activeWin = SP_MAIN_WINDOW_ACTIVE;
-//	return res;
-//}
-//
-//
-//void spManagerDestroy(SPGuiManager* src) {
-//	if (!src) {
-//		return;
-//	}
-//	if (src->activeWin == SP_GAME_WINDOW_ACTIVE) {
-//		spGameWindowDestroy(src->gameWin);
-//	}
-//	spMainWindowDestroy(src->mainWin);
-//	free(src);
-//}
-//
-//
-//void spManagerDraw(SPGuiManager* src) {
-//	if (!src) {
-//		return;
-//	}
-//	if (src->activeWin == SP_MAIN_WINDOW_ACTIVE) {
-//		spMainWindowDraw(src->mainWin);
-//	} else {
-//		spGameWindowDraw(src->gameWin);
-//	}
-//}
-//
-//
-//SP_MANAGER_EVENT handleManagerDueToMainEvent(SPGuiManager* src,
-//		SP_MAIN_EVENT event) {
-//	if (src == NULL ) {
-//		return SP_MANAGER_NONE;
-//	}
-//	if (event == SP_MAIN_NEW_GAME) {
-//		spMainWindowHide(src->mainWin);
-//		src->gameWin = spGameWindowCreate();
-//		if (src->gameWin == NULL ) {
-//			printf("Couldn't create game window\n");
-//			return SP_MANAGER_QUTT;
-//		}
-//		src->activeWin = SP_GAME_WINDOW_ACTIVE;
-//	}
-//
-//	else if (event == SP_MAIN_LOAD_GAME){}// TODO
-//
-//	if (event == SP_MAIN_EXIT) {
-//		return SP_MANAGER_QUTT;
-//	}
-//	return SP_MANAGER_NONE;
-//}
-//
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include "SPChessGUIManager.h"
+
+SPGuiManager* spManagerCreate() {
+	SPGuiManager* res = (SPGuiManager*) malloc(sizeof(SPGuiManager));
+	if (res == NULL ) {
+		return NULL ;
+	}
+	res->mainWin = spMainWindowCreate();
+	if (res->mainWin == NULL ) {
+		free(res);
+		return NULL ;
+	}
+    res->loadWin = NULL;
+    res->settingsWin = NULL;
+	res->gameWin = NULL;
+	res->activeWin = SP_MAIN_WINDOW_ACTIVE;
+	return res;
+}
+
+
+void spManagerDestroy(SPGuiManager* src) {
+	if (!src) {
+		return;
+	}
+	if (src->gameWin != NULL) {
+		spGameWindowDestroy(src->gameWin);
+	}
+    if (src->loadWin != NULL) {
+        spLoadWindowDestroy(src->loadWin);
+    }
+    if (src->settingsWin != NULL) {
+        spSettingsWindowDestroy(src->settingsWin);
+    }
+	spMainWindowDestroy(src->mainWin);
+	free(src);
+}
+
+
+void spManagerDraw(SPGuiManager* src) {
+	if (!src) {
+		return;
+	}
+	if (src->activeWin == SP_MAIN_WINDOW_ACTIVE) {
+		spMainWindowDraw(src->mainWin);
+	} else if (src->activeWin == SP_SETTINGS_WINDOW_ACTIVE){
+		spGameWindowDraw(src->settingsWin);
+	} else if (src->activeWin == SP_LOAD_WINDOW_ACTIVE){
+        spGameWindowDraw(src->loadWin);
+    } else if (src->activeWin == SP_GAME_WINDOW_ACTIVE){
+        spGameWindowDraw(src->gameWin);
+    }
+}
+
+
+SP_MANAGER_EVENT handleManagerDueToMainEvent(SPGuiManager* src, SP_MAIN_EVENT event) {
+	if (src == NULL ) {
+		return SP_MANAGER_NONE;
+	}
+    switch (event){
+        case SP_MAIN_NEW_GAME:
+            spMainWindowHide(src->mainWin);
+            src->settingsWin = spSettingsWindowCreate();
+            if (src->settingsWin == NULL ) {
+                printf("Couldn't create settings window\n");
+                return SP_MANAGER_QUTT;
+            }
+            src->activeWin = SP_SETTINGS_WINDOW_ACTIVE;
+            break;
+        case SP_MAIN_LOAD_GAME:
+            spMainWindowHide(src->mainWin);
+            src->settingsWin = spSettingsWindowCreate();
+            if (src->settingsWin == NULL ) {
+                printf("Couldn't create settings window\n");
+                return SP_MANAGER_QUTT;
+            }
+            src->activeWin = SP_LOAD_WINDOW_ACTIVE;
+            break;
+        case SP_MAIN_QUIT_GAME:
+            return SP_MANAGER_QUTT;
+        default:
+            break;
+    }
+    return SP_MANAGER_NONE;
+}
+
+
+SP_MANAGER_EVENT handleManagerDueToSettingsEvent(SPGuiManager* src, SP_SETTINGS_EVENT event) {
+    if (src == NULL) {
+        return SP_MANAGER_NONE;
+    }
+    switch (event){
+        case SP_SETTINGS_BACK:
+            spSettingsWindowDestroy(src->settingsWin);
+            src->activeWin = SP_MAIN_WINDOW_ACTIVE;
+            break;
+        case SP_SETTINGS_START:
+            spMainWindowHide(src->settingsWin);
+            src->settingsWin = spGameWindowCreate();
+            if (src->gameWin == NULL ) {
+                printf("Couldn't create settings window\n");
+                return SP_MANAGER_QUTT;
+            }
+            src->activeWin = SP_GAME_WINDOW_ACTIVE;
+            break;
+        default:
+            break;
+    }
+    return SP_MANAGER_NONE;
+}
+
+
 //SP_MANAGER_EVENT handleManagerDueToGameEvent(SPGuiManager* src,
 //		SP_GAME_EVENT event) {
 //	if (event == SP_GAME_EVENT_NONE || src == NULL ) {
