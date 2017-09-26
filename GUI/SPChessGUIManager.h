36ecfd7bc8f697618662393b7c7541437959476b
omerk1 <omerk1@mail.tau.ac.il>
2017-09-26 13:53:38 +0300

    Update SPChessGUIManager.h

--- GUI/SPChessGUIManager.h
+++ GUI/SPChessGUIManager.h
@@ -1,36 +1,38 @@
-//#ifndef SPCHESSGUIMANAGER_H_
-//#define SPCHESSGUIMANAGER_H_
-//#include <SDL.h>
-//#include "SPChessGameWin.h"
-//#include "SPChessMainWin.h"
-//
-//typedef enum{
-//	SP_MAIN_WINDOW_ACTIVE,
-//	SP_GAME_WINDOW_ACTIVE,
-//	SP_LOAD_WINDOW_ACTIVE,
-//	SP_SETTINGS_WINDOW_ACTIVE,
-//}ACTIVE_WINDOW;
-//
-//typedef enum{
-//	SP_MANAGER_QUTT,
-//	SP_MANAGER_NONE,
-//}SP_MANAGER_EVENT;
-//
-//typedef struct {
-//	SPMainWin* mainWin;
-//	SPSettingsWin* settingsWin;
-//	SPLoadWin* loadWin;
-//	SPGameWin* gameWin;
-//	ACTIVE_WINDOW activeWin;
-//} SPGuiManager;
-//
-//SPGuiManager* spManagerCreate();
-//
-//void spManagerDestroy(SPGuiManager* src);
-//
-//void spManagerDraw(SPGuiManager* src);
-//
-//SP_MANAGER_EVENT spManagerHandleEvent(SPGuiManager* src, SDL_Event* event) ;
-//
-//
-//#endif
+#ifndef SPCHESSGUIMANAGER_H_
+#define SPCHESSGUIMANAGER_H_
+#include <SDL.h>
+#include "SPChessMainWin.h"
+#include "SPChessSettingsWin.h"
+#include "SPChessLoadWin.h"
+#include "SPChessGameWin.h"
+
+typedef enum{
+	SP_MAIN_WINDOW_ACTIVE,
+	SP_GAME_WINDOW_ACTIVE,
+	SP_LOAD_WINDOW_ACTIVE,
+	SP_SETTINGS_WINDOW_ACTIVE,
+}ACTIVE_WINDOW;
+
+typedef enum{
+	SP_MANAGER_QUTT,
+	SP_MANAGER_NONE,
+}SP_MANAGER_EVENT;
+
+typedef struct {
+	SPMainWin* mainWin;
+	SPSettingsWin* settingsWin;
+	SPLoadWin* loadWin;
+	SPGameWin* gameWin;
+	ACTIVE_WINDOW activeWin;
+} SPGuiManager;
+
+SPGuiManager* spManagerCreate();
+
+void spManagerDestroy(SPGuiManager* src);
+
+void spManagerDraw(SPGuiManager* src);
+
+SP_MANAGER_EVENT spManagerHandleEvent(SPGuiManager* src, SDL_Event* event) ;
+
+
+#endif
