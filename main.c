f8c8ac931f7724b0ffa0a43ac3cee0c662749533
Ori Haviv <orihaviv@gmail.com>
2017-09-28 21:06:37 +0300

    console: no leaks in console. sdl in process

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 int main(int argc, char* argv[]) {
     if (argc == 0 || strcmp(argv[0], "-c") == 0){
     return executeConsoleGame();
-    } else if (strcmp(argv[0], "--g")) {
+    } else if (strcmp(argv[0], "-g")) {
         return executeGuiGame();
     }
 }
